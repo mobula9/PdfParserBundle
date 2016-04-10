a7ef7ccaeea66fd67be695bd6d00d882ccba15ba
Lucas CHERIFI <lucas@cherifi.info>
2016-04-10 23:06:39 +0200

    Refacto ParserCommand

--- Command/ParserCommand.php
+++ Command/ParserCommand.php
@@ -2,6 +2,7 @@
 
 namespace Kasifi\PdfParserBundle\Command;
 
+use Kasifi\PdfParserBundle\PdfParser;
 use Kasifi\PdfParserBundle\Util\ParseHelper;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Helper\Table;
@@ -19,6 +20,11 @@ use Symfony\Component\Yaml\Dumper;
  */
 class ParserCommand extends ContainerAwareCommand
 {
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+
     /**
      * Configure the command.
      */
@@ -40,21 +46,63 @@ class ParserCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->output = $output;
         $container = $this->getContainer();
         $pdfParser = $container->get('kasifi_pdfparser.pdf_parser');
         $pdfDirectoryPath = $container->getParameter('kasifi_pdfparser.pdf_directory_path');
 
         // Select processor
-        $processors = $pdfParser->getAvailableProcessors();
-        $processorId = $input->getArgument('processor');
-        if (!$processorId) {
-            $helper = $this->getHelper('question');
-            $question = new ChoiceQuestion('Which processor to use?', $processors);
-            $processorId = $helper->ask($input, $output, $question);
-        }
-        $pdfParser->setProcessor($processors[$processorId]);
+        $this->selectProcessor($input, $pdfParser);
 
         // Select file
+        $filePath = $this->selectFile($input, $pdfDirectoryPath);
+
+        // Parse
+        $rows = $pdfParser->parse($filePath);
+        $data = ParseHelper::inlineDates($rows->toArray());
+
+        // Write output
+        $this->writeOutput($input->getOption('format'), $data);
+
+        return;
+    }
+
+    /**
+     * @param                 $format
+     * @param                 $data
+     */
+    protected function writeOutput($format, $data)
+    {
+        switch ($format) {
+            case 'json':
+                $outputData = json_encode($data);
+                $this->output->write($outputData);
+                break;
+            case 'yml':
+                $dumper = new Dumper();
+                $outputData = $dumper->dump($data, 1);
+                $this->output->write($outputData);
+                break;
+            case 'console':
+                if (count($data)) {
+                    $table = new Table($this->output);
+                    $table
+                        ->setHeaders([array_keys($data[0])])
+                        ->setRows($data);
+                    $table->render();
+                }
+                break;
+        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param                $pdfDirectoryPath
+     *
+     * @return mixed
+     */
+    protected function selectFile(InputInterface $input, $pdfDirectoryPath)
+    {
         $filePath = $input->getArgument('file_path');
         if (!$filePath) {
             $helper = $this->getHelper('question');
@@ -67,39 +115,27 @@ class ParserCommand extends ContainerAwareCommand
             }
 
             $question = new ChoiceQuestion('Which file? Enter the key.', $files);
-            $filePath = $helper->ask($input, $output, $question);
+            $filePath = $helper->ask($input, $this->output, $question);
+
+            return $filePath;
         }
 
-        // Select format
-        $format = $input->getOption('format');
+        return $filePath;
+    }
 
-        // Parse
-        $rows = $pdfParser->parse($filePath);
-
-        $data = ParseHelper::inlineDates($rows->toArray());
-
-        // Write output
-        switch ($format) {
-            case 'json':
-                $outputData = json_encode($data);
-                $output->write($outputData);
-                break;
-            case 'yml':
-                $dumper = new Dumper();
-                $outputData = $dumper->dump($data, 1);
-                $output->write($outputData);
-                break;
-            case 'console':
-                if (count($data)) {
-                    $table = new Table($output);
-                    $table
-                        ->setHeaders([array_keys($data[0])])
-                        ->setRows($data);
-                    $table->render();
-                }
-                break;
+    /**
+     * @param InputInterface $input
+     * @param                $pdfParser
+     */
+    protected function selectProcessor(InputInterface $input, PdfParser $pdfParser)
+    {
+        $processors = $pdfParser->getAvailableProcessors();
+        $processorId = $input->getArgument('processor');
+        if (!$processorId) {
+            $helper = $this->getHelper('question');
+            $question = new ChoiceQuestion('Which processor to use?', $processors);
+            $processorId = $helper->ask($input, $this->output, $question);
         }
-
-        return;
+        $pdfParser->setProcessor($processors[$processorId]);
     }
 }
